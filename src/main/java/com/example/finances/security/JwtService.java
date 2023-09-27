67f4b7bd15b202760c068cb8657bd12d19dd02ce
hidoryy <gustavobrunodemelo@gmail.com>
2023-09-27 00:28:08 -0300

    chore: change token expiration time 1m -> 2h

--- src/main/java/com/example/finances/security/JwtService.java
+++ src/main/java/com/example/finances/security/JwtService.java
@@ -27,7 +27,7 @@ public class JwtService {
                 .setClaims(extraClaims)
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 2))
                 .signWith(getSignInKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
